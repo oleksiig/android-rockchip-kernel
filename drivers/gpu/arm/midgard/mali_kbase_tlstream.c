2e31cf4d4bc7a642dc599d285b3a17d582c1935b
Oleksii Gulchenko <alexey.gulchenko@gmail.com>
2019-09-16 18:10:23 +0300

    midgard: Fixed timers setup due changes in timers API
    Signed-off-by: Oleksii Gulchenko <alexey.gulchenko@gmail.com>

--- drivers/gpu/arm/midgard/mali_kbase_tlstream.c
+++ drivers/gpu/arm/midgard/mali_kbase_tlstream.c
@@ -28,6 +28,7 @@
 
 #include <mali_kbase.h>
 #include <mali_kbase_jm.h>
+#include <mali_kbase_utility.h>
 #include <mali_kbase_tlstream.h>
 
 /*****************************************************************************/
@@ -1037,17 +1038,17 @@ static void kbasep_tlstream_flush_stream(enum tl_stream_type stype)
 
 /**
  * kbasep_tlstream_autoflush_timer_callback - autoflush timer callback
- * @data:  unused
+ * @timer: unused
  *
  * Timer is executed periodically to check if any of the stream contains
  * buffer ready to be submitted to user space.
  */
-static void kbasep_tlstream_autoflush_timer_callback(unsigned long data)
+static void kbasep_tlstream_autoflush_timer_callback(struct timer_list *timer)
 {
 	enum tl_stream_type stype;
 	int                 rcode;
 
-	CSTD_UNUSED(data);
+	CSTD_UNUSED(timer);
 
 	for (stype = 0; stype < TL_STREAM_TYPE_COUNT; stype++) {
 		struct tl_stream *stream = tl_stream[stype];
@@ -1371,9 +1372,8 @@ int kbase_tlstream_init(void)
 
 	/* Initialize autoflush timer. */
 	atomic_set(&autoflush_timer_active, 0);
-	setup_timer(&autoflush_timer,
-			kbasep_tlstream_autoflush_timer_callback,
-			0);
+	kbase_timer_setup(&autoflush_timer,
+			kbasep_tlstream_autoflush_timer_callback);
 
 	return 0;
 }
