513a6aa4c87fdc7984df030ea72fd986d607b883
Oleksii Gulchenko <alexey.gulchenko@gmail.com>
2020-10-15 21:41:51 +0300

    Revert "FROMGIT: of: property: Add device link support for power-domains and hwlocks"
    This reverts commit eb2d94d24713366509023d16a7eef75909b5bf33.

--- drivers/of/property.c
+++ drivers/of/property.c
@@ -1202,8 +1202,6 @@ DEFINE_SIMPLE_PROP(mboxes, "mboxes", "#mbox-cells")
 DEFINE_SIMPLE_PROP(io_channels, "io-channel", "#io-channel-cells")
 DEFINE_SIMPLE_PROP(interrupt_parent, "interrupt-parent", NULL)
 DEFINE_SIMPLE_PROP(dmas, "dmas", "#dma-cells")
-DEFINE_SIMPLE_PROP(power_domains, "power-domains", "#power-domain-cells")
-DEFINE_SIMPLE_PROP(hwlocks, "hwlocks", "#hwlock-cells")
 DEFINE_SUFFIX_PROP(regulators, "-supply", NULL)
 DEFINE_SUFFIX_PROP(gpio, "-gpio", "#gpio-cells")
 
@@ -1235,8 +1233,6 @@ static const struct supplier_bindings of_supplier_bindings[] = {
 	{ .parse_prop = parse_io_channels, },
 	{ .parse_prop = parse_interrupt_parent, },
 	{ .parse_prop = parse_dmas, },
-	{ .parse_prop = parse_power_domains, },
-	{ .parse_prop = parse_hwlocks, },
 	{ .parse_prop = parse_regulators, },
 	{ .parse_prop = parse_gpio, },
 	{ .parse_prop = parse_gpios, },
